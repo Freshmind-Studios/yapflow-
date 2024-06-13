50b8043de1d9d131ebb9ead36372cf1754be5d4d
DanoHrCZ <daniel.hrecin@gmail.com>
2024-06-13 12:46:43 +0200

    backend rm

--- back-end/routes/sessionRouter.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const SessionController = require("../controllers/sessionController");
-
-router.ws("/", SessionController.connection);
-
-module.exports = router;
--- back-end/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const UserController = require("../controllers/userController");
-
-const { requireAuth } = require("../middlewares/authMiddleware");
-
-router.get("/:userId", requireAuth, UserController.get);
-
-module.exports = router;
